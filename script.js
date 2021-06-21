a49ee372b88a1c4d7e5fadc2368be0537e6f168d
ruhanbablu <ruhanbablu@gmail.com>
2021-06-21 16:36:03 +0600

    completed

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-const color = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const button = document.getElementById("button");
-const colors = document.getElementsByClassName("color");
+const color = document.querySelector(".color");
 
 button.addEventListener("click", function () {
     const randomNumber = getRandomNumber();
    
-    document.body.style.backgroundColor = color[randomNumber];
-    colors.textContent = color[randomNumber];
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
 
   });
 
-  console.log(button);
-
   function getRandomNumber() {
-    return Math.floor(Math.random() * color.length);
+    return Math.floor(Math.random() * colors.length);
   }
 
