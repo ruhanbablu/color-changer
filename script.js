0d25e007230cb7b93108e0dd2ce1e9b63216594a
ruhanbablu <ruhanbablu@gmail.com>
2021-06-21 16:44:30 +0600

    done

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ button.addEventListener("click", function () {
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 
+
   });
 
   function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
   }
+
+  
 
